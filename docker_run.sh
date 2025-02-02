25f88423f3be173ea5a19a99716a85565ebbf1de
take1014 <y.takehara1014@gmail.com>
2025-02-02 16:39:39 +0900

    update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,4 @@
 # use gpu0, name:take_torch2, memory:32GB, use_image:nvidia/cuda:pytorch2
-docker run --gpus '"device=0"' --name take_torch2 --memory=32g -it -v /home/take/fun:/app/fun nvidia/cuda:pytorch2 bash
+docker run --gpus '"device=0"' --name take_torch2 --memory=32g --shm-size=16g -it -v /home/take/fun:/app/fun nvidia/cuda:pytorch2 bash
 # use gpu 0, 1
 # docker run --gpus '"device=0,1"' --name take_torch2 --memory=32g -it nvidia/cuda:pytorch2 bash
